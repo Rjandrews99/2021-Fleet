8f877acbba758a0ea461dbea3b7fd66293b6f78b
Rjandrews99 <64694711+Rjandrews99@users.noreply.github.com>
2021-02-22 12:31:55 +0000

    lets see if this works

--- JS/hamburgericon.js
+++ JS/hamburgericon.js
@@ -1,3 +1,5 @@
-function hamburger(x) {
-    x.classList.toggle("change");
-  }
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
